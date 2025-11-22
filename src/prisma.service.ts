07cbaa21ae5b5f75c5f8ba1f4f03c0bf59eddb8e
Maxim Farizov <farnaks.net@gmail.com>
2025-11-22 23:11:19 +0200

    feat: docker finish

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -3,12 +3,10 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-    // Цей метод спрацьовує, коли NestJS запускає модуль
     async onModuleInit() {
         await this.$connect();
     }
 
-    // Цей метод спрацьовує, коли додаток вимикається (Graceful Shutdown)
     async onModuleDestroy() {
         await this.$disconnect();
     }
